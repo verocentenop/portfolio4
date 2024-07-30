f21707a5ffde12800d1a6bde682808629bddfd74
verocentenop <159185910+verocentenop@users.noreply.github.com>
2024-07-30 09:23:01 +0200

    Update arrayProjects.js

--- src/Projects/arrayProjects.js
+++ src/Projects/arrayProjects.js
@@ -1,6 +1,6 @@
 export const arrayProjects = [
   {
-    img: './src/assets/quexines.png',
+    img: 'https://res.cloudinary.com/dzqoduh1s/image/upload/v1722323993/quexines_ab7e6v.png',
     name: 'Quexines Films: Landing page',
     p: '#HTML    #CSS',
     link: 'https://vermillion-scone-41111d.netlify.app/',
@@ -8,13 +8,13 @@ export const arrayProjects = [
   },
   {
     name: 'CanonClick: Products sales page with filters',
-    img: './src/assets/canon-click.png',
+    img: 'https://res.cloudinary.com/dzqoduh1s/image/upload/v1722323896/canon-click_eior4l.png',
     p: '#HTML    #CSS    #JavaScript',
     link: 'https://venerable-pika-e1568f.netlify.app',
     code: 'https://github.com/verocentenop/proyecto2ok.git'
   },
   {
-    img: './src/assets/pinterest.png',
+    img: 'https://res.cloudinary.com/dzqoduh1s/image/upload/v1722323883/pinterest_jen4gi.png',
     name: 'Pinterest: Gallery with filters and search',
     p: '#HTML    #CSS    #JavaScript',
     link: 'http://localhost:5173/',

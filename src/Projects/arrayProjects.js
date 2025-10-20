36231b521c61577b63ac48dd5522f5d8d4219799
verocentenop <verocentenop@gmail.com>
2025-10-20 17:12:18 +0200

    fotos

--- src/Projects/arrayProjects.js
+++ src/Projects/arrayProjects.js
@@ -1,6 +1,6 @@
 export const arrayProjects = [
   {
-    img: 'https://res.cloudinary.com/dzqoduh1s/image/upload/v1722323993/quexines_ab7e6v.png',
+    img: 'https://res.cloudinary.com/dzqoduh1s/image/upload/v1760973060/quexines_dt9dyv.png',
     name: 'Quexines Films: Landing page',
     p: '#HTML    #CSS',
     link: 'https://quexines-films.netlify.app/',
@@ -17,7 +17,7 @@ export const arrayProjects = [
     img: './src/assets/games.png',
     name: 'VC Games: Arcade games',
     p: '#HTML    #CSS    #JavaScript',
-    link: 'https://vc-games.netlify.app',
+    link: 'https://res.cloudinary.com/dzqoduh1s/image/upload/v1760972780/logo_cpuagu.png',
     code: 'https://github.com/verocentenop/C-Games-proyecto5.git'
   }
 ]
